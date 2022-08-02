4b7fba80fd05386143d924022ce074fb1a48d857
Kanton99 <avolkov99@gmail.com>
2022-08-02 13:39:07 +0200

    Added canvas resizing, needs fixing

--- src/game.js
+++ src/game.js
@@ -19,13 +19,31 @@ class Game{
     // dirLight.position.set(-300,0,300)
     // level.level.add(dirLight);
     // dirLight.castShadow = true;
-    animate() {
-        requestAnimationFrame( this.animate );
+    animate(time) {
+        time+=0.001;
+        requestAnimationFrame( this.animate.bind(this) );
+
+        if(resizeRendererToDisplaySize(this.renderer)){
+            const canvas = this.renderer.domElement;
+            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            this.camera.updateProjectionMatrix();
+        }
         this.renderer.render(this.scene,this.camera);
         this.scene.getObjectByName("cube").rotation.y += 0.01;
     };
 
 }
 
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
 var game = new Game();
-game.animate();
+game.animate(0);
