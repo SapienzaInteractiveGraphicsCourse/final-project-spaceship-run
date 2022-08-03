01c5fc1f20324995d9a22110d3784a635d50ffa8
Kanton99 <avolkov99@gmail.com>
2022-08-03 17:39:42 +0200

    Added world class where the gameworld is

---
+++ src/World.js
@@ -0,0 +1,22 @@
+import { CommandCategories, CommandQueue } from "./Command";
+import * as THREE from "three"
+import GameObject from "./GameObject";
+class World extends GameObject{
+    constructor(){
+        this.scene = new THREE.Scene();
+        this.category==CommandCategories.None;
+    }
+
+    addChildren(child){
+        this.scene.children.push(child);
+    }
+
+    onCommand(command, deltaTime){
+        if(command.category==this.category){
+            command.action
+        }
+        this.scene.children.array.forEach(element => {
+            element.OnCommand(command, deltaTime);
+        });
+    }
+}
